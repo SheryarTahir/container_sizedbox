fbc33a651c9d7a9510b579e0ba3f93011f0d2a97
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2025-05-03 01:06:30 +0500

    Update button.dart

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -8,7 +8,7 @@ class Button extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent.shade100,
+        backgroundColor: Theme.of(context).primaryColor,
         title: const Text("Button"),
       ),
       body: Center(
@@ -16,19 +16,49 @@ class Button extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton(
-                style: const ButtonStyle(
-                    overlayColor: WidgetStatePropertyAll(Colors.white),
-                    elevation: WidgetStatePropertyAll(100),
-                    backgroundColor: WidgetStatePropertyAll(Colors.yellow)),
-                onPressed: () {},
-                child: const Text(
-                  "Press Me",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+              style: const ButtonStyle(
+                  overlayColor: WidgetStatePropertyAll(Colors.white),
+                  elevation: WidgetStatePropertyAll(100),
+                  backgroundColor: WidgetStatePropertyAll(Colors.yellow)),
+              onPressed: () {},
+              child: const Text(
+                "Press Me",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              width: 350,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      WidgetStatePropertyAll(Theme.of(context).primaryColor),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ))
+                ),
+                onPressed: () {
+                  print("Like");
+                },
+                child: const Text(
+                  "Click Me",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
