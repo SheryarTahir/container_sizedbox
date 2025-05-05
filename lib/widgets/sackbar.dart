4aeb250830e5ac3e9969636f9b6fcbc1e734bb69
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2025-05-05 23:18:57 +0500

    SnackBar code

--- lib/widgets/sackbar.dart
+++ lib/widgets/sackbar.dart
@@ -5,6 +5,27 @@ class SnackbarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text('Snack Bar'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              final snackbar = SnackBar(
+                action: SnackBarAction(
+                    label: 'Undo',
+                    textColor: Colors.blueGrey,
+                    onPressed: () {}),
+                behavior: SnackBarBehavior.floating,
+                duration: const Duration(milliseconds: 3000),
+                content: const Text('This is a Snackbar'),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackbar);
+            },
+            child: const Text('Show Snackbar')),
+      ),
+    );
   }
 }
