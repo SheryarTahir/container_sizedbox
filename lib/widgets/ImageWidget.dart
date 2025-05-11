c794e110399e440e2b0c81faaddc01b616f633ee
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2025-05-11 12:02:17 +0500

    Cached Network image code

--- lib/widgets/ImageWidget.dart
+++ lib/widgets/ImageWidget.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class Imagewidget extends StatelessWidget {
@@ -6,27 +7,35 @@ class Imagewidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('DOREMON'),
+        title: const Text('TOM & JERRY'),
         backgroundColor: Colors.blueGrey,
       ),
       body: Center(
         child: Container(
           height: 250,
           width: 250,
-          decoration: BoxDecoration(
-              boxShadow: const [
-                BoxShadow(
-                    blurRadius: 10, color: Colors.black, spreadRadius: 5.0),
-              ],
-              image: const DecorationImage(
-                image: NetworkImage(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_tAvMe23nZ_EFL6EDQG7mdbdI57ee5Dejnu9JFYRkNgV5Xf8qq6cb8U_jEhOHz665Z1U&usqp=CAU'),
-                fit: BoxFit.cover,
-              ),
-              color: Colors.pink,
-              borderRadius: BorderRadius.circular(20)),
+          child: CachedNetworkImage(
+            imageUrl:
+                'https://img.drz.lazcdn.com/static/pk/p/049fd4247bd194a276dafb0fb3848753.jpg_720x720q80.jpg',
+            fit: BoxFit.contain,
+            placeholder: (context, url) =>
+                const Center(child: CircularProgressIndicator()),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
+          // decoration: BoxDecoration(
+          //     boxShadow: const [
+          //       BoxShadow(
+          //           blurRadius: 10, color: Colors.black, spreadRadius: 5.0),
+          //     ],
+          //     image: const DecorationImage(
+          //       image: NetworkImage(
+          //           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_tAvMe23nZ_EFL6EDQG7mdbdI57ee5Dejnu9JFYRkNgV5Xf8qq6cb8U_jEhOHz665Z1U&usqp=CAU'),
+          //       fit: BoxFit.cover,
+          //     ),
+          //     color: Colors.pink,
+          //     borderRadius: BorderRadius.circular(20)),
         ),
       ),
     );
